3b21bc4850813b86ecbf482a8c604af2d27352c9
Mila Katajisto <mila.katajisto@gmail.com>
2019-02-04 22:58:03 +0200

    exercises for part 2

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Display = ({ countriesToShow }) => {
+const Display = ({ countriesToShow, setFilter, weatherData, fetchWeatherData, setWeatherIcon, weatherIcon }) => {
   if (countriesToShow.length ===1) {
     return (
-      <SingleCountry country={countriesToShow[0]}/>
+      <SingleCountry
+        country={countriesToShow[0]}
+        weatherData={weatherData}
+        fetchWeatherData={fetchWeatherData}
+        setWeatherIcon={setWeatherIcon}
+        weatherIcon={weatherIcon}
+      />
     )
   }
   else if (countriesToShow.length <= 10) {
     return (
-      <Countries countriesToShow={countriesToShow}/>
+      <Countries countriesToShow={countriesToShow} setFilter={setFilter}/>
     )
   }
   else {
@@ -19,8 +25,9 @@ const Display = ({ countriesToShow }) => {
   }
 }
 
-const SingleCountry = ({ country }) => {
+const SingleCountry = ({ country, weatherData, fetchWeatherData, setWeatherIcon, weatherIcon }) => {
   const flagAlt = "The flag of " + country.name
+  fetchWeatherData(country)
   return (
       <div>
       <h2>{country.name}</h2>
@@ -34,24 +41,39 @@ const SingleCountry = ({ country }) => {
       </ul>
       <br/>
       <h3>Flag</h3>
-      <img src={country.flag} alt={flagAlt} height="10%" width="10%" />
+      <img src={country.flag} alt={flagAlt} height="15%" width="15%" />
+      <h2>Weather in {country.capital}</h2>
+      <p><strong>Tempature: </strong>{weatherData.temp_c} celsius</p>
+      <img src={weatherIcon} alt="weather_icon"  />
+      <p><strong>Wind: </strong>{weatherData.wind_kph} kph direction {weatherData.wind_dir}</p>
       </div>
   )
 }
 
-const ListCountry = ({ country }) => {
+const ListCountry = ({ country, setFilter }) => {
   return (
-    <li>{country.name}</li>
+    <li>{country.name} <ShowButton country={country} setFilter={setFilter} /> </li>
   )
 }
 
-const Countries = ({countriesToShow}) =>
+const Countries = ({countriesToShow, setFilter}) =>
   countriesToShow.map(country =>
     <ListCountry
       key={country.name}
       country={country}
+      setFilter={setFilter}
     />
 )
+
+const ShowButton = ({ country, setFilter }) => {
+    const handleClick = () => {
+        setFilter(country.name.toLowerCase())
+    }
+    return (
+        <button onClick={handleClick}>Show</button>
+    )
+}
+
 
 const Language= ({ language }) => {
   return (
@@ -70,6 +92,8 @@ const Languages = ({ languages }) =>
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [weatherData, setWeatherData] = useState([])
+  const [weatherIcon, setWeatherIcon] = useState('')
 
   useEffect(() => {
     axios
@@ -86,6 +110,19 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const fetchWeatherData = (country) => {
+        useEffect(() => {
+
+            axios
+                .get(`http://api.apixu.com/v1/current.json?key=26366877576b409c8be202313190402&q=${country.capital}`)
+                .then(response => {
+                    setWeatherData(response.data.current)
+                    setWeatherIcon(response.data.current.condition.icon)
+                })
+
+        }, [])
+    }
+
   return (
     <div>
       <form>
@@ -94,7 +131,14 @@ const App = () => {
         </div>
       </form>
       <ul>
-          <Display countriesToShow={countriesToShow}/>
+          <Display
+            countriesToShow={countriesToShow}
+            setFilter={setFilter}
+            weatherData={weatherData}
+            fetchWeatherData={fetchWeatherData}
+            setWeatherIcon={setWeatherIcon}
+            weatherIcon={weatherIcon}
+          />
       </ul>
     </div>
   )
